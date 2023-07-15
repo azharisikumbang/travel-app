8650a0e65bc417be0abd7bd030da2e4af3533c8f
Azhari <azharisaputrasikumbang@gmail.com>
2023-07-16 04:28:44 +0700

    feat: pencarian map titik jemput

--- src/pages/info-pemesan.php
+++ src/pages/info-pemesan.php
@@ -35,8 +35,13 @@ html_require_component('navbar');
                             <input x-model="properties.form.kontak" type="text" class="w-full bg-white text-gray-700 font-sans font-normal outline outline-0 border-2 text-sm px-3 py-3 rounded-md border-gray-200 focus:border-gray-400">
                         </div>
                         <div class="w-full mb-4">
-                            <label for="" class="font-sans text-base text-gray-600 mb-2 block">Alamat Jemput</label>
-                            <textarea x-model="properties.form.titik_jemput" class="w-full bg-white text-gray-700 font-sans font-normal outline outline-0 border-2 text-sm px-3 py-3 rounded-md border-gray-200 focus:border-gray-400" cols="30" rows="5" x-text="properties.form.titik_jemput"></textarea>
+                            <div class="flex justify-between">
+                                <label for="" class="font-sans text-base text-gray-600 mb-2 block">
+                                    Titik Jemput
+                                </label>
+                                <button class="text-red-500" type="button" @click="showMap">atau pilih di peta</button>
+                            </div>
+                            <textarea placeholder="Ketik lokasi penjemputan sedetail mungkin..." x-model="properties.form.titik_jemput" class="w-full bg-white text-gray-700 font-sans font-normal outline outline-0 border-2 text-sm px-3 py-3 rounded-md border-gray-200 focus:border-gray-400" cols="30" rows="5" x-text="properties.form.titik_jemput"></textarea>
                         </div>
                         <div class="w-full mt-8">
                             <button type="submit" class="w-full text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-4 text-center mr-3 md:mr-0">Simpan dan Lanjutkan</button>
@@ -81,7 +86,41 @@ html_require_component('navbar');
             </div>
         </form>
     </div>
+    <!-- Main modal -->
+    <div x-show="properties.sites.show_map">
+        <div  tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+            <div class="relative w-full max-w-6xl mx-auto max-h-full">
+                <!-- Modal content -->
+                <div class="relative bg-white rounded-lg shadow">
+                    <!-- Modal header -->
+                    <div class="flex items-start justify-between p-4 border-b rounded-t">
+                        <h3 class="text-xl font-semibold text-gray-900">
+                            Pilih Lokasi
+                        </h3>
+                        <button @click="properties.sites.show_map = false" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center" data-modal-hide="defaultModal">
+                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                            </svg>
+                            <span class="sr-only">Close modal</span>
+                        </button>
+                    </div>
+                    <!-- Modal body -->
+                    <div class="p-6 space-y-6">
+                        <div id="map" class="w-full" style="height: 75vh" ></div>
+                    </div>
+                    <!-- Modal footer -->
+                    <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b">
+                        <button @click="properties.sites.show_map = false" data-modal-hide="defaultModal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Pilih</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="fixed top-0 left-0 right-0 z-40 w-full bg-black opacity-75 h-screen"></div>
+    </div>
 </main>
+<!-- Load the `mapbox-gl-geocoder` plugin. -->
+<script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.min.js"></script>
+<link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.css" type="text/css">
 <script type="text/javascript">
     document.addEventListener('alpine:init', () => {
         // @TODO: separate to file
@@ -106,6 +145,39 @@ html_require_component('navbar');
             },
             "displayNomorKursi": function() {
                 return this.properties.data.sesi_pesanan.list_kursi_dipesan.map(item => item.nomor_kursi).join(', ');
+            },
+            "showMap": function () {
+                this.properties.sites.show_map = true;
+
+                // const options = {
+                //     enableHighAccuracy: true,
+                //     timeout: 10000,
+                // };
+                //
+                // navigator.geolocation.getCurrentPosition(
+                //     this.mapSuccessCallback(),
+                //     this.mapErrorCalback,
+                //     options
+                // );
+            },
+            "closeMap": function () {
+                this.properties.sites.show_map = false;
+            },
+            "mapErrorCalback": function (err) {
+                console.log(err);
+            },
+            "mapSuccessCallback": function () {
+                // console.log(map)
+            },
+            "createMapboxGeocoderRequest": function (token, endpoint, long, lat) {
+                const url = `https://api.mapbox.com/geocoding/v5/${endpoint}/${long},${lat}.json`;
+
+                return axios.get(url, {
+                    params: {
+                        'types': 'poi',
+                        'access_token': token
+                    }
+                })
             }
         };
 
@@ -139,9 +211,11 @@ html_require_component('navbar');
                 "properties": {
                     "sites": {
                         "api_url": "<?= site_url() ?>",
+                        "show_map": false
                     },
                     "errors": {},
                     "data": <?= json_encode(['sesi_pesanan' => $detailPesanan->toArray()]) ?>,
+                    "map": {},
                     "form": {
                         "nama": "",
                         "kontak": "",
@@ -152,6 +226,70 @@ html_require_component('navbar');
                     this.properties.form.nama = this.properties.data.sesi_pesanan.nama_pemesanan;
                     this.properties.form.kontak = this.properties.data.sesi_pesanan.kontak_pemesanan;
                     this.properties.form.titik_jemput = this.properties.data.sesi_pesanan.titik_jemput;
+
+                    mapboxgl.accessToken = 'pk.eyJ1Ijoia3VteWFrdW0iLCJhIjoiY2tpcWhhaXRlMDlnMDJ0b3pwbTNvaDl4MSJ9.PhflJtxgdetwjV3eRmG6EQ';
+                    const map = new mapboxgl.Map({
+                        container: 'map',
+                        style: 'mapbox://styles/mapbox/streets-v12',
+                        center: [100.363256, -0.924759],
+                        zoom: 12
+                    });
+
+                    const mapMarker = new mapboxgl.Marker({
+                        color: "#db1f1f",
+                        draggable: true
+                    });
+
+                    mapMarker.on('dragend', () => {
+                        const lngLat = mapMarker.getLngLat();
+
+                        this.createMapboxGeocoderRequest(mapboxgl.accessToken, 'mapbox.places', lngLat.lng, lngLat.lat)
+                            .then(response => this.properties.form.titik_jemput = response.data.features[0].place_name)
+                            .catch(err => console.error(err));
+                    });
+
+                    map.on('click', (e) => {
+                        this.createMapboxGeocoderRequest(mapboxgl.accessToken, 'mapbox.places', e.lngLat.lng, e.lngLat.lat)
+                            .then(response => {
+                                this.properties.form.titik_jemput = response.data.features[0].place_name;
+                            })
+                            .catch(err => console.error(err));
+
+                        let lnglat = [e.lngLat.lng, e.lngLat.lat];
+
+                        map.flyTo({
+                            center: lnglat,
+                            zoom: 16
+                        });
+
+                        mapMarker.setLngLat(lnglat).addTo(map);
+                    })
+
+                    const geocoder = new MapboxGeocoder({
+                        accessToken: mapboxgl.accessToken,
+                        mapboxgl: mapboxgl,
+                        marker: false,
+                        placeholder: "Cari titik jemput.."
+                    });
+
+                    geocoder.on('result', e => {
+                        this.properties.form.titik_jemput = e.result['place_name_id-ID'];
+                        mapMarker.setLngLat(e.result.center).addTo(map);
+                    });
+
+                    map.addControl(geocoder);
+
+                    map.addControl(
+                        new mapboxgl.GeolocateControl({
+                            positionOptions: {
+                                enableHighAccuracy: true
+                            },
+                            trackUserLocation: true
+                        })
+                    );
+
+                    const nav = new mapboxgl.NavigationControl();
+                    map.addControl(nav, 'top-left');
                 }
             })
         );
--- src/templates/public.php
+++ src/templates/public.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
     <script src="//unpkg.com/alpinejs" defer></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src='https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.js'></script>
+    <link href='https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.css' rel='stylesheet' />
 </head>
 <body>
 <?php require_once $content; ?>
