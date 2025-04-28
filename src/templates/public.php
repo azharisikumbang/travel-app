6ff30d22f8e2ab5de706cd1b56a22a36475f2b8d
Kum <azharisaputrasikumbang@gmail.com>
2025-04-28 10:32:06 +0700

    fix: use library locally

--- src/templates/public.php
+++ src/templates/public.php
@@ -1,15 +1,18 @@
 <!doctype html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="//unpkg.com/alpinejs" defer></script>
+    <script src="<?= public_url() ?>assets/js/alpine.js" defer></script>
     <script src="https://cdn.tailwindcss.com"></script>
-    <script src='https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.js'></script>
-    <link href='https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.css' rel='stylesheet' />
+    <script src='<?= public_url() ?>assets/js/mapbox-gl.js'></script>
+    <link href='<?= public_url() ?>assets/css/mapbox-gl.css' rel='stylesheet' />
 </head>
+
 <body>
-<?php require_once $content; ?>
-<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    <?php require_once $content; ?>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 </body>
+
 </html>
